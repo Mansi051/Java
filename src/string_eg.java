fc65afef627dd209387eae9a5743f4ddfcd39336
Mansi051 <mansiarora1701@gmail.com>
2026-02-09 00:47:36 +0530

    string example

---
+++ src/string_eg.java
@@ -0,0 +1,8 @@
+public class string_eg {
+    public static void main(String args[]){
+        String str1="Hello";
+        String str2=new String("Java");
+        System.out.println(str1);
+        System.out.println(str2);
+    }
+}
